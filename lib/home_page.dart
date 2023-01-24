be831f833529cabee3323e6182bc8b4ae149fb64
yazeed.ali <yazeed.ali@seera.sa>
2023-01-24 23:34:43 +0300

    edit home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,26 +4,45 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weather_app/api/api.dart';
 import 'package:weather_app/api/constans.dart';
+import 'package:weather_app/model/weather.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var fetchNetworkCall = Api().get(weatherUrl, {
+      'q': 'amman',
+      'lang': 'eng',
+      'mode': 'json',
+      'appid': '2e5045c7038a055bebcd451b9fbc88f2',
+      'units': 'metric'
+    });
+
     return Scaffold(
-      body: InkWell(
-          onTap: () {
-            Api().get(weatherUrl, {
-              'q': 'amman',
-              'lang': 'eng',
-              'mode': 'json',
-              'appid': '2e5045c7038a055bebcd451b9fbc88f2',
-              'units': 'metric'
-            });
-          },
-          child: const Center(
-            child: Text("Get"),
-          )),
+      body: FutureBuilder<WeatherResult?>(
+        future: fetchNetworkCall, // async work
+        builder:
+            (BuildContext context, AsyncSnapshot<WeatherResult?> snapshot) {
+          // if data still fethcing the resutl return loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          // to show error if data return null
+          print(snapshot.error);
+
+          // get data
+          WeatherResult? data = snapshot.data;
+
+          // bulid your ui
+          return Scaffold(
+            body: Container(
+              child: Center(child: Text(data?.id.toString() ?? "")),
+            ),
+          );
+        },
+      ),
     );
   }
 }
